5b3686aa04fc6e17d9a0ba66efb5e868e1b77ac0
Leonardo <leosud099@gmail.com>
2020-08-27 18:03:51 -0600

    added default colors and splash screen

---
+++ constants/colors.js
@@ -0,0 +1,7 @@
+  
+export default {
+    primary: '#FF8A00', // darker orange
+    accent: '#FFC978',  // ligher orange
+    warning: '#FF0000', // strong red
+    success: '#4F8D00'  // darker green
+ };
