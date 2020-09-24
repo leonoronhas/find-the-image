7ba9a3d0041995809f0f058cf4448abf52a844ef
Leonardo <leosud099@gmail.com>
2020-09-23 20:39:35 -0600

    updated with one color only design

--- components/ViewBackground.js
+++ components/ViewBackground.js
@@ -11,7 +11,7 @@ import Color from "../constants/colors";
 const ViewBackground = (props) => (
   <SafeAreaView style={{ ...styles.container, ...props.style }}>
     <StatusBar hidden />
-    <View style={{ ...styles.body, ...props.style }}>{props.children}</View>
+    <View style={{...props.style}}>{props.children}</View>
   </SafeAreaView>
 );
 
@@ -19,19 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Color.primary,
-  },
-  body: {
-    flex: 1,
-    backgroundColor: Color.accent,
-    margin: 10,
-    borderRadius: 20,
-    // Shadow props iOS only
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.3,
-    // Elevation props Android only
-    elevation: 8,
   },
 });
 
