88bc7f66793bf4ad5c727101fa3dd09b70a86190
Leonardo <leosud099@gmail.com>
2020-09-23 20:39:07 -0600

    added open sans fonts

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import React from "react";
 import { AppLoading } from "expo";
 import { useFonts } from "expo-font";
 
-import BeforeGameScreen from "./screens/BeforeGameScreen";
+import GetStartedScreen from "./screens/GetStartedScreen";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     mouseMemoirs: require("./assets/fonts/MouseMemoirs-Regular.ttf"),
+    openSans: require("./assets/fonts/OpenSans-Regular.ttf"),
+    openSansBold: require("./assets/fonts/OpenSans-Bold.ttf")
   });
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
-  return <BeforeGameScreen />;
+  return <GetStartedScreen />;
 }
