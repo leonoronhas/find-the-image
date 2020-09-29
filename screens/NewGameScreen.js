b1da3769f0fc69dd6bd66c69bfac9be8e860cbf1
Leonardo <leosud099@gmail.com>
2020-09-28 23:01:38 -0600

    New Game Screen design updated

--- screens/NewGameScreen.js
+++ screens/NewGameScreen.js
@@ -1,74 +1,88 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, StatusBar, SafeAreaView } from "react-native";
 import { scale, verticalScale } from "react-native-size-matters";
 
 import Color from "../constants/colors";
-import ViewBackground from "../components/ViewBackground";
 import DefaultBodyText from "../components/DefaultBodyText";
+
 import DefaultButton from "../components/DefaultButton";
+import { RFPercentage } from "react-native-responsive-fontsize";
 
 const NewGameScreen = ({ params }) => {
   return (
-    <ViewBackground>
-      <View style={styles.container}>
-        <View style={styles.categoryContainer}>
-          <View style={styles.options}>
-            <DefaultBodyText>Choose number category:</DefaultBodyText>
-            <DefaultButton>Choose</DefaultButton>
-          </View>
-          <View>
-            <DefaultBodyText>Choose difficulty:</DefaultBodyText>
-            <DefaultButton>Choose</DefaultButton>
-          </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <View style={styles.categoryContainer}>
+        <View style={styles.options}>
+          <DefaultBodyText style={styles.text}>
+            CHOOSE CATEGORY:
+          </DefaultBodyText>
+          <DefaultButton>CATEGORY</DefaultButton>
         </View>
-        <View style={styles.text}>
-          <DefaultBodyText>Are you ready?</DefaultBodyText>
-        </View>
-        <View style={styles.readyContainer}>
-          <DefaultButton style={styles.ready}>I'm ready!</DefaultButton>
-        </View>
-        <View style={styles.homeContainer}>
-          <DefaultButton style={styles.home}>Home</DefaultButton>
+        <View style={styles.options}>
+          <DefaultBodyText style={styles.text}>
+            CHOOSE DIFFICULTY:
+          </DefaultBodyText>
+          <DefaultButton>DIFFICULTY</DefaultButton>
         </View>
       </View>
-    </ViewBackground>
+      <View style={{ marginTop: 20 }}>
+        <DefaultBodyText>ARE YOU READY?</DefaultBodyText>
+      </View>
+      <View style={styles.readyContainer}>
+        <DefaultButton style={styles.home}>
+          <DefaultBodyText style={styles.homeText}>HOME</DefaultBodyText>
+        </DefaultButton>
+        <DefaultButton style={styles.ready}>I'M READY!</DefaultButton>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    backgroundColor: Color.primary,
   },
   categoryContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+    width: "100%",
+    height: verticalScale(160),
   },
   options: {
     alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 10,
+    justifyContent: "space-around",
+    height: verticalScale(120),
   },
   text: {
     alignItems: "center",
     marginVertical: 20,
+    color: Color.accent,
+    fontFamily: "openSansBold",
   },
   readyContainer: {
     flex: 1,
+    flexDirection: "row",
     alignItems: "center",
     marginVertical: 20,
+    width: "100%",
+    height: verticalScale(160),
+    justifyContent: "space-around",
   },
   ready: {
-    backgroundColor: Color.done,
-  },
-  homeContainer: {
-    flex: 1,
-    marginHorizontal: 10,
-    marginVertical: 10,
+    backgroundColor: "white",
   },
   home: {
     width: scale(120),
     height: verticalScale(50),
+    backgroundColor: Color.accent,
+  },
+  homeText: {
+    color: Color.warning,
+    fontSize: RFPercentage(2.5),
   },
 });
 
