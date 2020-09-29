ae7d427ebb62eb5dfc83f15e2506e6152d81fb61
Leonardo <leosud099@gmail.com>
2020-09-28 22:42:39 -0600

    Updated design for Choose difficulty screen completed

--- screens/DifficultyScreen.js
+++ screens/DifficultyScreen.js
@@ -1,107 +1,118 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import { scale, verticalScale } from "react-native-size-matters";
 import { RFPercentage } from "react-native-responsive-fontsize";
 
 import Color from "../constants/colors";
-import ViewBackground from "../components/ViewBackground";
+import DefaultTitleText from "../components/DefaultTitleText";
 import DefaultBodyText from "../components/DefaultBodyText";
 import DefaultButton from "../components/DefaultButton";
 
 const DifficultyScreen = () => {
   return (
-    <ViewBackground>
-      <View style={styles.container}>
-        <View style={styles.textContainer}>
-          <DefaultBodyText style={styles.text}>
-            Choose dufficulty:
-          </DefaultBodyText>
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <View style={styles.textContainer}>
+        <DefaultTitleText style={styles.text}>
+          Choose difficulty:
+        </DefaultTitleText>
+      </View>
+      <View style={styles.difficultyContainer}>
+        <View style={styles.options}>
+          <DefaultButton style={styles.buttonOption}>
+            <DefaultBodyText style={styles.difficultyEasyText}>
+              EASY{"      "}
+              <DefaultBodyText style={styles.difficultyEasyText2}>
+                30 seconds 40 numbers
+              </DefaultBodyText>
+            </DefaultBodyText>
+          </DefaultButton>
         </View>
-        <View style={styles.difficultyContainer}>
-          <View style={styles.options}>
-            <DefaultButton style={styles.buttonEasy}>Easy</DefaultButton>
-            <DefaultBodyText style={styles.difficultyText}>
-              30 seconds to match the
+        <View style={styles.options}>
+          <DefaultButton style={styles.buttonOption}>
+            <DefaultBodyText style={styles.difficultyMediumText}>
+              MEDIUM{"      "}
+              <DefaultBodyText style={styles.difficultyMediumText2}>
+                30 seconds 60 numbers
+              </DefaultBodyText>
             </DefaultBodyText>
-            <DefaultBodyText style={styles.difficultyText}>
-              number among
-            </DefaultBodyText>
-            <DefaultBodyText style={styles.difficultyText}>
-              40 numbers
-            </DefaultBodyText>
-          </View>
-          <View style={styles.options}>
-            <DefaultButton style={styles.buttonMedium}>Medium</DefaultButton>
-            <DefaultBodyText style={styles.difficultyText}>
-              30 seconds to match the
-            </DefaultBodyText>
-            <DefaultBodyText style={styles.difficultyText}>
-              number among
-            </DefaultBodyText>
-            <DefaultBodyText style={styles.difficultyText}>
-              60 numbers
-            </DefaultBodyText>
-          </View>
-          <View style={styles.options}>
-            <DefaultButton style={styles.buttonHard}>Hard</DefaultButton>
-            <DefaultBodyText style={styles.difficultyText}>
-              20 seconds to match the
-            </DefaultBodyText>
-            <DefaultBodyText style={styles.difficultyText}>
-              number among
-            </DefaultBodyText>
-            <DefaultBodyText style={styles.difficultyText}>
-              80 numbers
-            </DefaultBodyText>
-          </View>
+          </DefaultButton>
         </View>
-        <View style={styles.doneContainer}>
-          <DefaultButton style={styles.done}>Done!</DefaultButton>
+        <View style={styles.options}>
+          <DefaultButton style={styles.buttonOption}>
+            <DefaultBodyText style={styles.difficultyHardText}>
+              HARD{"      "}
+              <DefaultBodyText style={styles.difficultyHardText2}>
+                20 seconds 80 numbers
+              </DefaultBodyText>
+            </DefaultBodyText>
+          </DefaultButton>
         </View>
       </View>
-    </ViewBackground>
+      <View style={styles.doneContainer}>
+        <DefaultButton style={styles.done}>DONE!</DefaultButton>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    backgroundColor: Color.primary,
   },
   difficultyContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
-    justifyContent: "space-evenly",
+    justifyContent: "space-around",
     alignItems: "center",
     marginVertical: 20,
+    width: "100%",
   },
   options: {
     textAlign: "center",
     alignItems: "center",
+    width: scale(150),
   },
   difficultyText: {
-    fontSize: RFPercentage(3.5),
+    fontSize: RFPercentage(3),
   },
   textContainer: {
     alignItems: "center",
     marginVertical: 20,
   },
   text: {
-    fontSize: RFPercentage(6),
+    fontSize: RFPercentage(8),
   },
-  buttonEasy: {
-    width: scale(120),
-    height: verticalScale(50),
-    backgroundColor: Color.success,
+  buttonOption: {
+    width: scale(160),
+    height: verticalScale(140),
+    backgroundColor: "white",
   },
-  buttonMedium: {
-    width: scale(120),
-    height: verticalScale(50),
-    backgroundColor: Color.warning,
+  difficultyEasyText: {
+    fontSize: RFPercentage(4),
+    color: Color.done,
   },
-  buttonHard: {
-    width: scale(120),
-    height: verticalScale(50),
-    backgroundColor: Color.danger,
+  difficultyEasyText2: {
+    fontSize: RFPercentage(3),
+    color: Color.done,
+  },
+  difficultyMediumText: {
+    fontSize: RFPercentage(4),
+    color: Color.warning,
+  },
+  difficultyMediumText2: {
+    fontSize: RFPercentage(3),
+    color: Color.warning,
+  },
+  difficultyHardText: {
+    fontSize: RFPercentage(4),
+    color: Color.danger,
+  },
+  difficultyHardText2: {
+    fontSize: RFPercentage(3),
+    color: Color.danger,
   },
   doneContainer: {
     flex: 1,
@@ -111,7 +122,7 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
   done: {
-    backgroundColor: Color.done,
+    backgroundColor: "white",
     width: scale(120),
     height: verticalScale(50),
   },
