e29af836c78ea6b949c8b9a033e38df7832b5d0c
Leonardo <leosud099@gmail.com>
2020-10-06 19:00:19 -0600

    Record keeping file created

---
+++ data/playerRecord.js
@@ -0,0 +1,13 @@
+let record = [];
+
+export const setRecordBack = (value) => {
+  difficultyOption = value;
+};
+
+export const setRecordFront = (value) => {
+  difficultyOption = value;
+};
+
+export const getRecord = () => {
+  return record;
+};
